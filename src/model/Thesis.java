aab73f17aea849e886f2dbadbcb1d69467a0cffb
merari <merariurbano0322@gmail.com>
2026-03-13 16:25:39 -0500

    model/Thesis

---
+++ src/model/Thesis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author MERARI URBANO
+ */
+public class Thesis extends Material {
+
+    public Thesis(String code, String title, String author, String category) {
+        super(code, title, author, category);
+    }
+}
